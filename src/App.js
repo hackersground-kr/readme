2f09019d9b28ac60e8948af757ce448ad8784755
Jinsoo1004 <jinsoo1004@proton.me>
2023-06-21 22:40:33 +0900

    set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./routers/router";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
