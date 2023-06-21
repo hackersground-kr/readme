5db844655e17201151e89f0666ca49a0bf9f4c16
Chanhuyk Lee <cksgur0612@dgsw.hs.kr>
2023-06-21 22:48:12 +0900

    init

---
+++ src/test/java/com/alt/readmegateway/ReadmeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alt.readmegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadmeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
