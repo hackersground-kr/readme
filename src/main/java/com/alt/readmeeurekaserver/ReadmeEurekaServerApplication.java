332af61481949fe5e1085c46a1c61a88a3e2588a
Chanhuyk Lee <cksgur0612@dgsw.hs.kr>
2023-06-21 21:08:47 +0900

    init

---
+++ src/main/java/com/alt/readmeeurekaserver/ReadmeEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.alt.readmeeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadmeEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadmeEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alt/readmeeurekaserver/ReadmeEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alt.readmeeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadmeEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
