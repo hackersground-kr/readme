7d7eabfbcfd6774eff232578d5bc2ba5a8b2c9ff
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-21 23:05:40 +0900

    commit

---
+++ app/src/main/java/com/hackathon/readmeandroid/view/activity/account/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hackathon.readmeandroid.view.activity.account
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hackathon.readmeandroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
