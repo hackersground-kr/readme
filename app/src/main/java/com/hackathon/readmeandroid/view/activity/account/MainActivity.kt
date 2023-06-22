2b5386480f3da8948f1a671c3a03f401f5e03198
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-22 19:30:29 +0900

    :bug 버그 해결

--- app/src/main/java/com/hackathon/readmeandroid/view/activity/account/MainActivity.kt
+++ app/src/main/java/com/hackathon/readmeandroid/view/activity/account/MainActivity.kt
@@ -6,19 +6,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.hackathon.readmeandroid.databinding.ActivityMainBinding
 
-
-class MainActivity : AppCompatActivity() {
-    companion object {
-        lateinit var instance: MainActivity
-        fun ApplicationContext() : Context {
-            return instance.applicationContext
-        }
-    }
-
-    init{
-        MainActivity.instance = this
-    }
-
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
