5e2da7c77e697267a4a5d6e3141c400fe2af0906
Jinsoo1004 <jinsoo1004@proton.me>
2023-06-22 09:26:06 +0900

    🔨 :: School 메인페이지

---
+++ src/components/School/EditIntroduce/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import pencil from "../../../assets/img/pencil.png";
+
+import * as S from "./style";
+
+export const EditIntroduce = () => {
+  return (
+    <S.Box>
+      <S.GroupWrapper>
+        <S.Group>
+          <S.Overlap>
+            <S.RectangleWrapper>
+              <S.Rectangle />
+            </S.RectangleWrapper>
+            <S.Div />
+            <S.TextWrapper>
+              대구 구지면에 위치한 마이스터고등학교입니다
+              <br />
+              대구 구지면에 위치한 마이스터고등학교입니다
+              <br />
+              대구 구지면에 위치한 마이스터고등학교입니다
+              <br />
+              대구 구지면에 위치한 마이스터고등학교입니다
+              <br />
+              대구 구지면에 위치한 마이스터고등학교입니다
+              <br />
+              대구 구지면에 위치한 마이스터고등학교입니다
+            </S.TextWrapper>
+          </S.Overlap>
+          <S.H1>학교 설명</S.H1>
+          <S.Image alt="Image" src={pencil} />
+          <S.OverlapGroupWrapper>
+            <S.OverlapGroup>
+              <S.TextWrapper2>수정</S.TextWrapper2>
+            </S.OverlapGroup>
+          </S.OverlapGroupWrapper>
+        </S.Group>
+      </S.GroupWrapper>
+    </S.Box>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { EditInformation } from "../components/School/EditInformation";
-
+import { EditIntroduce } from "../components/School/EditIntroduce";
+import { SchoolSideBar } from "../components/School/SchoolSideBar";
 
 export const Home = () => {
   return (
     <div>
-        <EditInformation />
+      <SchoolSideBar />
+      <EditInformation />
+      <EditIntroduce />
     </div>
   );
 };
