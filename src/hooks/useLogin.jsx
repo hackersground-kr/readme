39add6e0110f78501139a0749314a64265478ef2
Jinsoo1004 <jinsoo1004@proton.me>
2023-06-23 01:48:52 +0900

    Auto stash before rebase of "origin/web"

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -25,20 +25,19 @@ export const useLogin = () => {
       if (email && password) {
         try {
           //   console.log(email, password, URL);
-          const response = await axios.post(
-            URL,
-            {
-              email: email,
-              password: password,
-            },
-            { Credentials: true },
-          );
+          const response = await axios.post(URL, {
+            email: email,
+            password: password,
+          });
 
           console.log(response);
-          ToastSuccess("회원가입에 성공하였습니다.");
+          const { accessToken, refreshToken } = response.data.data;
+          localStorage.setItem("accessToken", accessToken);
+          document.cookie = `refreshToken=${refreshToken}`;
+          ToastSuccess("로그인에 성공하였습니다.");
           navigate("/login");
         } catch (error) {
-          ToastError("회원가입에 실패했습니다.\n관리자에게 문의해주세요");
+          ToastError("로그인에 실패했습니다.\n관리자에게 문의해주세요");
         }
       } else {
         ToastWarning("모든 항목을 입력해주세요");
