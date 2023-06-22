3fcaabc9f5eb03314385164bb0116e7931da084d
Jinsoo1004 <jinsoo1004@proton.me>
2023-06-23 05:57:16 +0900

    NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import Toast from "../lib/Toast";
+import { ToastError } from "../lib/Toast";
 
 const NotFound = () => {
   const navigate = useNavigate();
   useEffect(() => {
-    Toast.fire({
-      icon: "error",
-      title: "존재하지 않는 페이지입니다",
-    });
+    ToastError("존재하지않는 페이지입니다.");
     navigate("/");
   }, [navigate]);
   return <></>;
