3b3b9ed5da9bb15cabdd20d1c4d1b2208f9535d3
Jinsoo1004 <jinsoo1004@proton.me>
2023-06-22 10:35:46 +0900

    🔨 :: 파일분리

--- src/components/School/SchoolSideBar/index.jsx
+++ src/components/School/SchoolSideBar/index.jsx
@@ -1,63 +1,17 @@
 import React from "react";
-import styled from "styled-components";
 
-// 스타일 컴포넌트를 사용하여 스타일을 정의합니다.
-const SidebarContainer = styled.div`
-  position: fixed;
-  height: 100%;
-  width: 200px;
-  background-color: #ffffff;
-  padding: 20px;
-  box-shadow: 3px 3px 10px 2px #00000040;
-`;
-
-const Logo = styled.div`
-  font-size: 36px;
-  font-weight: bold;
-  margin-bottom: 20px;
-`;
-
-const MenuItems = styled.div`
-  position: absolute;
-  top: 400px;
-  left: 80px;
-`;
-
-const MenuItem = styled.div`
-  font-size: 24px;
-  margin-bottom: 20px;
-  cursor: pointer;
-`;
-
-const BottomMenu = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  margin-left: 10px;
-  width: 90%;
-  height: 70px;
-  background-color: #ff5f5f;
-  margin-bottom: 50px;
-  border-radius: 20px;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px;
-  color: white;
-  cursor: pointer;
-`;
+import * as S from "./style";
 
 export const SchoolSideBar = () => {
   return (
-    <SidebarContainer>
-      <Logo>ReadMe</Logo>
-      <MenuItems>
-        <MenuItem>학생승인</MenuItem>
-        <MenuItem>학생추천</MenuItem>
-        <MenuItem>기업목록</MenuItem>
-      </MenuItems>
-      <BottomMenu>로그아웃</BottomMenu>
-    </SidebarContainer>
+    <S.SidebarContainer>
+      <S.Logo>ReadMe</S.Logo>
+      <S.MenuItems>
+        <S.MenuItem>학생승인</S.MenuItem>
+        <S.MenuItem>학생추천</S.MenuItem>
+        <S.MenuItem>기업목록</S.MenuItem>
+      </S.MenuItems>
+      <S.BottomMenu>로그아웃</S.BottomMenu>
+    </S.SidebarContainer>
   );
 };
