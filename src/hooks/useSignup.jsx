808bc7698fef9fd0f24d791f2c257e520cd2a0a5
Jinsoo1004 <jinsoo1004@proton.me>
2023-06-22 16:39:54 +0900

    🔨 :: 회원가입

--- src/hooks/useSignup.jsx
+++ src/hooks/useSignup.jsx
@@ -8,8 +8,13 @@ export const useSignup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [selectOption, setSelectOption] = useState("");
+  const [selectedType, setSelectedType] = useState("");
+  const [userNumber, setUserNumber] = useState("");
+  const [userType, setUserType] = useState("");
+  const [userAdress, setUserAdress] = useState("");
 
-  const URL = `dad.com`;
+  const School_URL = `https://readme-api-manager.azure-api.net/user/signup/school`;
+  const Company_URL = `https://readme-api-manager.azure-api.net/user/signup/company`;
 
   const onSignUpChange = useCallback((e) => {
     if (e.target.name === "email") {
@@ -17,35 +22,77 @@ export const useSignup = () => {
     } else if (e.target.name === "password") {
       setPassword(e.target.value);
     } else if (e.target.name === "school") {
-        setSelectOption(e.target.value);
+      setSelectOption(e.target.value);
+      setUserType("school");
     } else if (e.target.name === "company") {
-        setSelectOption(e.target.value);
+      setSelectOption(e.target.value);
+      setUserType("company");
+    } else if (e.target.id === "H") {
+      setSelectedType(e.target.id);
+    } else if (e.target.id === "u") {
+      setSelectedType(e.target.id);
+    } else if (e.target.id === "U") {
+      setSelectedType(e.target.id);
+    } else if (e.target.id === "N") {
+      setSelectedType(e.target.id);
+    } else if (e.target.name === "student") {
+      setUserNumber(e.target.value);
+    } else if (e.target.name === "adress") {
+      setUserAdress(e.target.value);
     }
   }, []);
 
-  
   const onSignUpSubmit = useCallback(
     async (e) => {
       e.preventDefault();
-      
+
       if (email && password && selectOption) {
-        try {
-          console.log(email, password, selectOption, URL);
-          const response = await axios.post(URL, {
-            email: email,
-            password: password,
-          });
-          console.log(response);
-          ToastSuccess("회원가입에 성공하였습니다.");
-          navigate("/login");
-        } catch (error) {
-          ToastError("회원가입에 실패했습니다.\n관리자에게 문의해주세요");
+        if (userType === "school") {
+          try {
+            // console.log(email, password, selectOption, selectedType, userNumber, URL);
+            const response = await axios.post(School_URL, {
+              headers: {
+                "Ocp-Apim-Subscription-Key": "39f12545fd02451aa9e865348e0e4151",
+              },
+              name: selectOption,
+              email: email,
+              password: password,
+              location: userAdress,
+              school_type: selectedType,
+              student: userNumber,
+            });
+            console.log(response);
+            ToastSuccess("회원가입에 성공하였습니다.");
+            navigate("/login");
+          } catch (error) {
+            ToastError("회원가입에 실패했습니다.\n관리자에게 문의해주세요");
+          }
+        } else if (userType === "campany") {
+          try {
+            // console.log(email, password, selectOption, selectedType, userNumber, URL);
+            const response = await axios.post(Company_URL, {
+              headers: {
+                "Ocp-Apim-Subscription-Key": "39f12545fd02451aa9e865348e0e4151",
+              },
+              name: selectOption,
+              email: email,
+              password: password,
+              location: userAdress,
+              income: 100000000,
+              employee: userNumber,
+            });
+            console.log(response);
+            ToastSuccess("회원가입에 성공하였습니다.");
+            navigate("/login");
+          } catch (error) {
+            ToastError("회원가입에 실패했습니다.\n관리자에게 문의해주세요");
+          }
         }
       } else {
         ToastWarning("모든 항목을 입력해주세요");
       }
     },
-    [email, password, selectOption, URL, navigate],
+    [email, password, selectOption, URL, navigate, selectedType, userNumber, userType],
   );
   return { onSignUpChange, email, password, onSignUpSubmit };
 };
